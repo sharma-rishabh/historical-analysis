43a8acbba72746ec52fefb85a6dab19ff4f82286
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2024-07-25 10:37:43 +0530

    Fix breakout-with-analysis

--- invest_assist/commands/breakout_with_analysis.py
+++ invest_assist/commands/breakout_with_analysis.py
@@ -6,14 +6,24 @@ from jugaad_data.nse import stock_df
 from invest_assist.company_list import listings
 from invest_assist.analyzer import Analyzer
 from invest_assist.models import Portfolio
-from .utils import get_breakout, get_current_price, get_stop_loss, read_portfolio, strategy_class, validate_path
+from .utils import (
+    get_breakout,
+    get_current_price,
+    get_stop_loss,
+    read_portfolio,
+    strategy_class,
+    validate_path,
+)
 from .historical_analysis import print_analysis_result
 from .buy import print_buying_result
 
-def get_buying_data(portfolio: Portfolio, symbol:str, strategy_name: str):
+
+def get_buying_data(portfolio: Portfolio, symbol: str, strategy_name: str):
     current_price = get_current_price(symbol)
     strategy = strategy_class[strategy_name]["class"]
-    historical_analysis_result = Analyzer(symbol, portfolio, strategy, 3650, stock_df).analyse()
+    historical_analysis_result = Analyzer(
+        symbol, portfolio, strategy, 3650, stock_df
+    ).analyse()
     stop_loss = get_stop_loss(symbol, strategy_name)
 
     return portfolio.buy_stock(
@@ -25,7 +35,7 @@ def get_buying_data(portfolio: Portfolio, symbol:str, strategy_name: str):
         strategy_name,
         date.today(),
         historical_analysis_result,
-        True
+        True,
     )
 
 
@@ -61,7 +71,6 @@ def get_buying_data(portfolio: Portfolio, symbol:str, strategy_name: str):
 def breakout_with_analysis(
     strategy_name: str, all: bool, n: int, portfolio: click.types.File, years: int
 ):
-    
     """
     Get a list of all the companies that broke out today for a particular strategy along with their historical-analysis.
     """
@@ -74,26 +83,22 @@ def breakout_with_analysis(
     symbols = df.head(n)["Symbol"].tolist()
 
     click.secho("Filtering breakouts: ", bold=True)
-    with click.progressbar(symbols) as syms:  
+    with click.progressbar(symbols) as syms:
         breakouts = [symbol for symbol in syms if get_breakout(symbol, strategy_name)]
 
     strategy = strategy_class[strategy_name]["class"]
     parsed_pf = read_portfolio(portfolio)
 
     click.secho("\n\nRunning Analysis: ", bold=True)
-    with click.progressbar(breakouts) as breaks: 
-        results = [Analyzer(symbol, parsed_pf, strategy, 365 * years).analyse()
-                   for symbol in breaks]
+    with click.progressbar(breakouts) as breaks:
+        results = [
+            Analyzer(symbol, parsed_pf, strategy, 365 * years, stock_df).analyse()
+            for symbol in breaks
+        ]
 
         results.sort(key=lambda x: x.returns)
 
-        
-
     for result in results:
         print_analysis_result(result.symbol, strategy_name, result)
-        holding = get_buying_data(
-            parsed_pf,
-            result.symbol,
-            strategy_name
-        )
+        holding = get_buying_data(parsed_pf, result.symbol, strategy_name)
         print_buying_result(holding, True)
